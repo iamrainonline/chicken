61a85b5a1587fe43aee89f91a04e9925932cb549
Cristian <cristiancandidatu@hotmail.com>
2024-12-07 13:49:49 +0200

    tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,72 +1,62 @@
 import React from "react";
-import "./styles/app.scss";
 import {
-   createBrowserRouter as Router,
-   RouterProvider,
-   Outlet,
+  createBrowserRouter as Router,
+  RouterProvider,
+  Outlet,
 } from "react-router-dom";
-// Pages
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Single from "./pages/Single";
-import Write from "./pages/Write";
-import Home from "./pages/Home";
-import Error from "./pages/Error";
+
+import Home from "./components/Home";
+import Login from "./components/Auth/Login";
+// Header
+import Navbar from "./components/Header/Navbar";
+// Utils
+import Error from "./components/Utils/Error";
 // Components
 import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 
 const Layout = () => {
-   return (
-      <div>
-         <Navbar />
-         <Outlet />
-         <Footer />
-      </div>
-   );
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 const router = Router([
-   {
-      path: "*",
-      element: <Error />,
-   },
-   {
-      path: "/",
-      element: <Layout />,
-      children: [
-         {
-            path: "/",
-            element: <Home />,
-         },
-         {
-            path: "/post/:id",
-            element: <Single />,
-         },
-         {
-            path: "/write",
-            element: <Write />,
-         },
-      ],
-   },
-   {
-      path: "/register",
-      element: <Register />,
-   },
-   {
-      path: "/login",
-      element: <Login />,
-   },
+  {
+    path: "*",
+    element: <Error />,
+  },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <div>some thing</div>,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
 const App = () => {
-   return (
-      <div className="app">
-         <div className="container">
-            <RouterProvider router={router} />
-         </div>
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
       </div>
-   );
+    </div>
+  );
 };
 
 export default App;
--- frontend/src/components/Utils/Error.jsx
+++ frontend/src/components/Utils/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Error = () => {
-   return <div>This page does not exist </div>;
+  return <div>Error</div>;
 };
 
 export default Error;
